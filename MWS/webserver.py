2f22e45a48d260d769df94524365b23c5faa8677
NerdCodex <subburakesh82@gmail.com>
2023-11-09 19:38:34 +0530

    Added URL Arguments

--- MWS/webserver.py
+++ MWS/webserver.py
@@ -12,20 +12,36 @@ class mws():
         request = connection.recv(1024).decode()
         if not request: return
         method, path, _ = request.split(' ', 2)
-        if path in self.routes:
-            response = self.routes[path]()
-            connection.send(response.encode())
-        
-        else:
-            self.not_found(connection)
-        connection.close()
-    
-    def not_found(self, connection):
+        for route, handler in self.routes.items():
+            ispresent, pos = self.match(path, route)
+            if ispresent:
+                route_part = route.split("/")[pos]
+                path_part = path.split("/")[pos]
+                urlargs = {route_part:path_part}
+                response = handler(urlargs)
+                connection.send(response.encode())
+                connection.close()
+                return
+
+    def match(self, path, route):
+        path_parts = path.split("/")
+        route_parts = route.split("/")
+        if len(path_parts) != len(route_parts): return False, None
+        for path_part, route_part in zip(path_parts, route_parts):
+            if route_part.startswith("{") and route_part.endswith("}"):
+                pos = route_parts.index(route_part)
+                continue
+            if path_part != route_part:
+                return False, 0
+        return True, pos
+
+
+    def not_found(self, connection) -> None:
         response = 'HTTP/1.1 404 Not Found\nContent-Type: text/html\n\nNot Found'
         connection.send(response.encode())
         connection.close()
     
-    def run(self, host="127.0.0.1", port=8080):
+    def run(self, host="127.0.0.1", port=8080)-> None:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((host, port))
         server.listen()
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from EWS.webserver import mws
-from EWS.render_template import *
+from MWS.webserver import mws
+from MWS.render_template import *
 
 app = mws()
 
-def home():
-    return render_template("templates/index.html", var={"name":"Rakesh"})
+def home(urlargs):
+    name = urlargs.get("{name}")
+    return render_template("templates/index.html", var={"name":name})
 
-app.add_route("/", home)
+app.add_route("/{name}", home)
 
 if __name__ == "__main__":
     app.run()
